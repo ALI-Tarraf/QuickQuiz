f2d066c6938f054386a9d773a1a9c3ea6076b831
ALI-Tarraf <alitarraf707@gmail.com>
2025-06-03 12:05:45 +0300

    Test 2

--- frontend/src/components/RoleProtectedRoute.jsx
+++ frontend/src/components/RoleProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 const RoleProtectedRoute = ({ roles, children }) => {
-  const user = { role: "student" };
+  const user = { role: "teacher" };
   return roles.includes(user.role) ? children : <Navigate to="/unauthorized" />;
 };
 
